00ed7dab63ce9a772c4f9c63834b038bd80c5a58
Mateus Sousa <sousa.programador@gmail.com>
2023-09-17 13:47:45 -0300

    refactor infra layer

--- src/infrastructure/order/repository/sequelize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequelize/order.repository.spec.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from 'sequelize-typescript';
-import Order from '../../domain/checkout/entity/order';
-import OrderItem from '../../domain/checkout/entity/order_item';
-import Customer from '../../domain/customer/entity/customer';
-import Address from '../../domain/customer/value-object/address';
-import Product from '../../domain/product/entity/product';
-import CustomerModel from '../db/sequelize/model/customer.model';
-import CustomerRepository from './customer.repository';
-import ProductModel from '../db/sequelize/model/product.model';
-import ProductRepository from './product.repository';
-import OrderItemModel from '../db/sequelize/model/order-item.model';
-import OrderModel from '../db/sequelize/model/order.model';
+import Order from '../../../../domain/checkout/entity/order';
+import OrderItem from '../../../../domain/checkout/entity/order_item';
+import Customer from '../../../../domain/customer/entity/customer';
+import Address from '../../../../domain/customer/value-object/address';
+import Product from '../../../../domain/product/entity/product';
+import CustomerModel from '../../../customer/repository/sequelize/customer.model';
+import CustomerRepository from '../../../customer/repository/sequelize/customer.repository';
+import ProductModel from '../../../product/repository/sequelize/product.model';
+import ProductRepository from '../../../product/repository/sequelize/product.repository';
+import OrderItemModel from './order-item.model';
+import OrderModel from './order.model';
 import OrderRepository from './order.repository';
 
 describe('Order repository test', () => {
--- src/infrastructure/order/repository/sequelize/order.repository.ts
+++ src/infrastructure/order/repository/sequelize/order.repository.ts
@@ -1,8 +1,8 @@
-import OrderRepositoryInterface from '../../domain/checkout/repository/order-repository.interface';
-import Order from '../../domain/checkout/entity/order';
-import OrderItem from '../../domain/checkout/entity/order_item';
-import OrderItemModel from '../db/sequelize/model/order-item.model';
-import OrderModel from '../db/sequelize/model/order.model';
+import OrderRepositoryInterface from '../../../../domain/checkout/repository/order-repository.interface';
+import Order from '../../../../domain/checkout/entity/order';
+import OrderItem from '../../../../domain/checkout/entity/order_item';
+import OrderItemModel from './order-item.model';
+import OrderModel from './order.model';
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async update(entity: Order): Promise<void> {
